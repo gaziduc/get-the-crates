9fd9ffcc4c5683435a50006d8b35a00c9cdc0c49
David Ghiassi <david.ghiassi@gmail.com>
2021-08-09 20:25:11 +0200

    Improved tooltip (color, size, alignment...)

--- Assets/Scripts/UI/Tooltip.cs
+++ Assets/Scripts/UI/Tooltip.cs
@@ -16,7 +16,7 @@ public class Tooltip : MonoBehaviour
 
    private void Update()
    {
-      transform.position = Input.mousePosition;
+      transform.position = Input.mousePosition + new Vector3(20f, 0f, 0f);
    }
 
    public void ShowTooltip(string tooltipString)
@@ -24,7 +24,7 @@ public class Tooltip : MonoBehaviour
       gameObject.SetActive(true);
 
       tooltipText.text = tooltipString;
-      float paddingTextSize = 4f;
+      float paddingTextSize = 10f;
       Vector2 backgroundSize = new Vector2(tooltipText.preferredWidth + paddingTextSize * 2f, tooltipText.preferredHeight + paddingTextSize * 2f);
       backgroundRectTransform.sizeDelta = backgroundSize;
    }
